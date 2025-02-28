e1ec4457d2c85b6b4f7bc41fb57744529471557d
drewtanapon <drew1234.t@gmail.com>
2025-03-01 01:19:34 +0700

    edit

--- Mobile Application/my-app/App.js
+++ Mobile Application/my-app/App.js
@@ -1,60 +1,19 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, ImageBackground } from 'react-native';
-import Constants from 'expo-constants';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginScreen from "./LoginScreen"; 
+import RegisterScreen from "./RegisterScreen";
 
-// Import components
-import FaceView from './components/FaceView';
-import { Card } from 'react-native-paper';
-import * as Font from 'expo-font';
+const Stack = createStackNavigator();
 
 export default function App() {
-  const [fontLoaded, setFontLoaded] = React.useState(false);
-  const [fontsLoaded] = Font.useFonts({
-    chakapetch: require('./assets/ChakraPetch-Regular.ttf'),
-  });
-
-  const bgImage = require('./assets/images.jpg');
-
   return (
-    <ImageBackground source={bgImage} style={styles.background}>
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>Work 5: Animal camera</Text>
-        <Card style={styles.card}>
-          <FaceView />
-        </Card>
-        <Text style={[styles.paragraph, { fontSize: 17 }]}>
-          test
-        </Text>
-      </View>
-    </ImageBackground>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingTop: Constants.statusBarHeight,
-    width: '100%',
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 27,
-    fontWeight: 'bold',
-    textAlign: 'bottom',
-    fontFamily: 'chakapetch',
-    color: '#fff', 
-  },
-  card: {
-    width: '90%',
-    padding: 10,
-  },
-});
