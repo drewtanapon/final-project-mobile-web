686cab73a2a068821d7c1db6fad2cbff6e55aab9
kitiphong.p <113675800+kitiphongP@users.noreply.github.com>
2025-03-02 03:09:14 +0700

    re path web

--- Mobile Application/my-app/HomeScreen.js
+++ Mobile Application/my-app/HomeScreen.js
@@ -57,6 +57,8 @@ const HomeScreen = ({ navigation }) => {
       const uid = auth.currentUser?.uid;
       if (!uid) return;
 
+      const subjectId = data;
+
       const classDocRef = doc(db, "Classes", data);
       const classDocSnap = await getDoc(classDocRef);
 
